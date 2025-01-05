4f3ea7497d500a3037f6e3922eac7322f11cbf0a
Michael Omonedo <rukkyomonedo@gmail.com>
2025-01-05 13:42:44 +0100

    installed mongodb and created a db connection

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,15 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasks.js");
 
 // routes
-router.route("/").get((req, res) => {
-    res.send("Task Manager App")
-})
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
-module.exports = router
+module.exports = router;
