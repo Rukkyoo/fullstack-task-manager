d667ceb86ee04026f5020007bf25aac2231d5851
Michael Omonedo <rukkyomonedo@gmail.com>
2025-01-06 22:44:21 +0100

    worked on controllers

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const express = require("express")
-const app = express()
-const tasks = require("./routes/tasks")
+const connectDB = require("./db/connect");
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const { connect } = require("mongoose");
+require("dotenv").config();
 
 // routes
-app.get("/hello", (req, res) =>{
-    res.send("welcome to my task manager app")
-})
-
+app.get("/hello", (req, res) => {
+  res.send("welcome to my task manager app");
+});
 
 // Using the directory below and placing the task route there
-app.use("/api/v1/tasks", tasks)
+app.use("/api/v1/tasks", tasks);
 
-const port = 3000
+const port = 3000;
 
-app.listen (port, () => {
-    console.log(`server is listening on port ${port}`)
-})
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
